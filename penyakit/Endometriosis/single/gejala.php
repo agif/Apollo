9a83b89186f4e281a5815325a09f9b8fe1906c0a
agif <alpenizme@gmail.com>
2018-02-15 16:41:44 +0700

    New For Penyakit

---
+++ penyakit/Endometriosis/single/gejala.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Klinik Sentosa</title>
+    <meta name="language" content="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0,target-densitydpi=device-dpi, user-scalable=no">
+    <!-- <link href="http://dakhoahoancau.vn/img/favicon.png" rel="shortcut icon" type="image/x-icon"> -->
+    <meta name="geo.position" content="10.751806,106.684639">
+    <meta name="geo.placename" content="TPHCM">
+    <meta name="geo.region" content="VN-65">
+    <meta name="robots" content="noodp,index,follow">
+    <meta name="revisit-after" content="1 days">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta itemprop="name" content="">
+    <meta itemprop="description" content="">
+    <!-- Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="../../../css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/mdb.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/demo.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/jquery.mmenu.all.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/css.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/JS5.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/style.css">
+</head>
+<body>
+
+<?php include ('../../../layouts/header.php'); ?>
+        
+
+<div id="mm-0" class="mm-page mm-slideout">
+    <div class="w650">
+		<div class="banner">
+			<img class="img-fluid banner" src="../../../img/banner/doktor-1.jpg" alt="Klinik umum tim medis lengkap sempurna">
+		</div>
+        <section class="tranggiua m_content">
+        	<div class="content_h">
+				<div class="m_motbenh">
+					<div class="breadcrumb_main">
+						<div class="breadcrumb">
+							<a href="../../../" itemprop="url">
+								<span itemprop="title" class="badge unique-color white-text z-depth-0">Home</span>
+							</a>
+							<span class="breadcrumb-span">
+                                <i class="fa fa-angle-right"></i>
+                            </span>
+						</div>
+						<!-- <div class="breadcrumb">
+							<a href="../../" itemprop="url">
+								<span itemprop="title" class="badge unique-color white-text z-depth-0">Penyakit</span>
+							</a>
+							<span class="breadcrumb-span">
+                                <i class="fa fa-angle-right"></i>
+                            </span>
+						</div> -->
+						<div class="breadcrumb">
+							<a href="../" itemprop="url">
+								<span itemprop="title" class="badge unique-color white-text z-depth-0">Endometriosis</span>
+							</a>
+							<span class="breadcrumb-span">
+                                <i class="fa fa-angle-right"></i>
+                            </span>
+						</div>
+						<div class="breadcrumb">
+							<a href="#in-here" itemprop="url">
+								<span itemprop="title" class="badge unique-color white-text z-depth-0">Gejala</span>
+							</a>
+						</div>
+						<div class="clear"></div>
+					</div>
+					<h4 class="h4-post h4-responsive"><i class="fa fa-th-large" aria-hidden="true"></i> Gejala Endometriosis</h4>
+					<span class="post-time">
+						Tanggal Diposting : 10-01-2018 - Tampilan : 667
+					</span><br><br>
+					<div class="content_chitiet">
+						<p class="font-weight-bold">
+							Gejala Utama Pada Penyakit Endometriosis
+						</p>
+						<p>
+							Apa yang menjadi gejala utama endometriosis adalah adanya rasa nyeri pada panggul (pelvic) yang sering dikaitkan dengan periode menstruasi. Meski pada kebanyakan wanita akan mengalami kram saat menstruasi, wanita dengan penyakit endomestriosis biasanya akan merasakan nyeri haid yang jauh lebih buruk dari biasanya. Dan cenderung akan merasakan rasa sakit yang meningkat seiring berjalannya waktu.
+						</p>
+						<p>
+							Gejala endometriosis sangat bervariasi dan beberapa dari wanita mengalami gejala yang ringan namun ada juga yang memiliki gejala sedang sampai parah. Dan tingkat keparahan rasa nyeri tidak menunjukkan tingkat stadium kondisinya. Kemungkinan memiliki bentuk penyakit ringan namun mengalami rasa sakit yang menyiksa. Kemungkinan memiliki bentuk yang parah dan hanya sedikit ketidaknyamanan. Nyeri pada panggul (pelvic) adalah gejala endometriosis yang paling umum.
+						</p>
+						<p class="special-type">
+							<a href="#" style="text-align: center;"><span class="special-rgb"><strong><img src="../../../img/icon/special-icon.gif"></strong></span>
+							</a><a href="#"><span style="color:#0000FF"><strong>>>> pelajari lebih lanjut mengenai gejala endometriosis </strong></span><span style="color:#FF0000"><strong> &gt;&gt;&gt; [klik disini] <<< </strong></span></a>
+						</p>
+
+						<p class="font-weight-bold">
+							Adapun Tanda Dan Gejala Yang Umum Penyakit Endometriosis Meliputi:
+						</p>
+					</div>
+
+					<div class="container">
+						<div class="chitietbaiviet-lienquan container card">
+							<ul>
+								<li><big class="red-text">Dismenore (Periode Nyeri)</big>
+									Rasa nyeri pada panggul (pelvic) dan kram dapat terasa sebelum menstruasi dan waktunya akan lama dari beberapa hari ke masa menstruasi. Dan kmeungkinan akan memiliki sakit pada perut.
+								</li>
+								<li><big class="red-text">Rasa Nyeri Saat Hubungan Intim</big>
+									Merasakan rasa nyeri selama atau setelah berhubungan seksual sering terjadi pada penyakit endometriosis.
+								</li>
+								<li><big class="red-text">Rasa Nyeri Saat Buang Air Besar (BAB) Atau Buang Air Kecil (BAK Atau Kencing)</big>
+									Kemungkinan besar akan mengalami gejala endometriosis selama menstruasi.
+								</li>
+								<li><big class="red-text">Terjadinya Pendarahan Yang Berlebihan</big>
+									Kemungkinan mengalami mengalami periode yang sangat berat sesekali atau yang disebut menorrhagia atau pendarahan antar menstruasu (menometrorrhagia).
+								</li>
+								<li><big class="red-text">Ketidaksuburan</big>
+									Endometriosis pertama kali didiagnosis pada beberapa wanita yang sedang mengobati infertilitas atau kemandulan.
+								</li>
+								<li><big class="red-text">Gejala Endometriosis Yang Lainnya</big>
+									Kemungkinan akan mengalami kelelahan, diare, konstipasi, perut kembung dan mual, terutama pada masa mentsruasi.
+								</li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="content_chitiet">
+						<p>
+							Pada keparahan tingkat rasa sakit yang belum tentu merupakan indikator yang dapat diandalkan mengenai tingkat kodisi yang sebenarnya. Beberapa dari wanita dengan penyakit endometriosisi yang ringan memiliki rasa sakit yang hebat sementara yang lainnya dengan gejala endometriosis kadang akan keliru untuk mengenali kondisi seperti ini yang dapat menyebabkna rasa nyeri pada pelvis (panggul) seperti penyakit radang panggul (PID) atau kista ovarium (indung telur). Gejalanya akan sama dengan kondisi yang menyebabkan terjadinya serangan penyakit diare, konstipasi dan kram pada perut.
+						</p>
+						<p class="special-type">
+							<a href="#" style="text-align: center;"><span class="special-rgb"><strong><img src="../../../img/icon/special-icon.gif"></strong></span>
+							</a><a href="#"><span style="color:#0000FF"><strong>>>> untuk lebih jelas?  </strong></span><span style="color:#FF0000"><strong> &gt;&gt;&gt; [klik disini untuk konsultasi] <<< </strong></span></a>
+						</p>
+						<p class="font-weight-bold">
+							Gejala Endometriosis Yang Kemungkinan Muncul Termasuk:
+						</p>
+					</div>
+
+					<div class="container">
+						<div class="chitietbaiviet-lienquan container card">
+							<ul>
+								<li>Kram pada perut atau sakit pada punggung bagian bawah saat menstruasi</li>
+								<li>Kram saat menstruasi yang parah dan tak henti-hentinya</li>
+								<li>Nyeri saat buang air besar (BAB) atau buang air kecil (BAK atau kencing) terutama saat menstruasi</li>
+								<li>Pendarahan yang tidak normal atau berat selama menstruasi</li>
+								<li>Hubungan seksual yang menyakitkan</li>
+								<li>Sulitnya untuk hamil</li>
+								<li>Nyeri pada panggul (pelvic) jangka panjang</li>
+								<li>Bermasalah dengan usus dan kencing termasuk rasa sakit seperti diare, konstipasi dan kembung</li>
+								<li>Urine berdarah</li>
+								<li>Mual dan muntah</li>
+								<li>Kelelahan</li>
+								<li>Bercak atau berdarah antar menstruasi</li>
+								<li>Ketidaksuburan</li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="content_chitiet">
+						<p>
+							Rasa nyeri sesaat sebelum, selama atau setelah menstruasi adalah gejala yang paling umum. Bagi beberapa wanita rasa sakit ini kemungkinan akan melumpuhkan dan kemungkinan akan terjadi selama atau setelah berhubungan seksual, atau saat buang air besar (BAB) atau buang air kecil (BAK atau kencing). Namun terkadang dapat menyebabkan rasa skait yang terus menerus berlanjut di daerah panggul dan punggung bawah. Pada kebanyakan wanita dengan penyakit endometriosis juga memiliki gejala yang ringan atau tidak ada gejala. 
+						</p>
+						<p class="special-type">
+							<a href="#" style="text-align: center;"><span class="special-rgb"><strong><img src="../../../img/icon/special-icon.gif"></strong></span>
+							</a><a href="#"><span style="color:#0000FF"><strong></strong></span><span style="color:#FF0000"><strong> &gt;&gt;&gt; [klik disini untuk konsultasi] <<< </strong></span></a>
+						</p>
+						<p class="font-weight-bold">
+							Fakta Tentang Gejala Endometriosis
+						</p>
+						<p>
+							Seiring jaringan yang terus menebal, rusak, dan respon hormom siklus haid dan berdarah selama siklus menstruasi, bentuk endometriosis jauh didalam tubuh. Jaringan parut dan bentuk adhesi ini bisa menyebabkan perpaduan organ dan perubahan anatomis. Rasa sakit adalah indikasi endometriosis yang paling umum, namun tingkat keparahan rasa sakit tidak selalu berkorelasi dengan tingkat penyakitnya. 
+						</p>
+					</div>
+
+					<div class="container">
+						<div class="chitietbaiviet-lienquan container card">
+							<ul>
+								<li>Endometriosis dapat mempengaruhi antara 6 dan 10 wanita usia reproduksi diseluruh dunia</li>
+								<li>Kondisi seperti ini tampaknya ada pada janin yang sedang berkembang namun kadar estrogen selama pubertas diperkirakan memicu gejalanya</li>
+								<li>Gejalanya pada umumnya terajdi pada masa reproduksi</li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="content_chitiet">
+						<p>
+							Jaringan endometriosis terdiri dari kelenjar, sel darah, dan jaringan ikat. Biasanya tumbuh didalam rahim, untuk mempersiapkan lapisan rahim untuk ovulasi. Implan endometriosis adalah pembentukan jaringan endometium yang tumbuh di lokasi di luar rahim. Ketika mereka tumbuh di luar rahim disebut endometriosis. Bisa berkembang dimana saja didalam tubuh, tapi biasanya terjadi didaerah pelvis (panggul) seperti:
+						</p>
+					</div>
+
+					<div class="container">
+						<div class="chitietbaiviet-lienquan container card">
+							<ul>
+								<li>Ovarium (indung telur)</li>
+								<li>Saluran tuba</li>
+								<li>Peritoneum </li>
+								<li>Kelenjar getah bening</li>
+							</ul>
+						</div>
+					</div>
+
+					<div class="content_chitiet">
+						<p class="special-type">
+							<a href="#" style="text-align: center;"><span class="special-rgb"><strong><img src="../../../img/icon/special-icon.gif"></strong></span>
+							</a><a href="#"><span style="color:#0000FF"><strong>>>> butuh bantuan chat online </strong></span><span style="color:#FF0000"><strong> &gt;&gt;&gt; [klik disini] <<< </strong></span></a>
+						</p>
+						<p class="text-justify">
+							<strong class="red-text"><b>Klinik Sentosa</b></strong> adalah salah satu klinik spesialis penyakit kulit dan kelamin yang berada di Jakarta, Indonesia. Menangani setiap permasalahan penyakit kulit dan kelamin seperti penyakit menular seksual (PMS), infeksi menular seksual (IMS), Andrologi dan Ginekologi. Didukung oleh dokter ahli dan peralatan medis yang modern guna membantu proses pengobatan  penyakit kelamin.
+						</p>		
+					</div>
+
+					<blockquote class="blockquote bq-warning">
+						<p class="text-muted">Disclaimer: Hasil dapat berbeda dari masing-masing individu.</p>
+					</blockquote>
+
+					<p class="jumbotron" style="color:red;">*Note: Efek pengobatan tergantung pada kondisi fisik masing-masing orang</p>
+					<div class="clear20"></div>
+					<ul class="list_doc">
+						<div class="card card-header pinked-low mb-3">
+							<h5 class="h5-responsive white-text">Posting Terkait</h5>
+						</div>
+						<li>
+							<a href="cara.php">
+								<img class="img-thumbnail img-responsive img-fluid" src="../thumbnail/Cara-Pengobatan-Endometriosis.jpg" align="left">
+								Cara Mengangani Endometriosis
+							</a>
+						</li>
+	                    <hr>
+	                    <li>
+							<a href="klinik.php">
+								<img class="img-thumbnail img-responsive img-fluid" src="../thumbnail/Klinik-Pengobatan-Endometriosis.jpg" align="left">
+									Klinik Terbaik Yang Menangani Endometriosis
+							</a>
+						</li>
+						<hr>
+						<li>
+							<a href="biaya.php">
+								<img class="img-thumbnail img-responsive img-fluid" src="../thumbnail/Biaya-Pengobatan-Endometriosis.jpg" align="left">
+									Biaya Seputar Endometriosis
+							</a>
+						</li>
+						<hr>
+						<li>
+							<a href="penyebab.php">
+								<img class="img-thumbnail img-responsive img-fluid" src="../thumbnail/Penyebab-Endometriosis.jpg"
+								 align="left" >
+								 	Penyebab Endometriosis
+							</a>
+						</li>
+						<hr>
+					</ul>
+				</div>
+			</div>
+		</section>
+	</div>
+</div>
+
+<?php include ('../../../layouts/footer.php'); ?>
+
+<script type="text/javascript" src="../../../js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="../../../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../../../js/popper.min.js"></script>
+<script type="text/javascript" src="../../../js/request.js"></script>
+<script type="text/javascript" src="../../../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../../../js/mdb.min.js"></script>
+<script type="text/javascript" src="../../../js/jquery.mmenu.min.all.js"></script>
+<script type="text/javascript" src="../../../js/style.js"></script>
+<script type="text/javascript">
+$(function() {
+    $('#menu').mmenu({
+        extensions: ['effect-slide-menu', 'penyakithadow'],
+        searchfield: false,
+        counters: true,
+        navbar: {
+            title: '<a href=".">Klinik Sentosa</a>'
+        },
+        navbars: [{
+            position: 'top',
+            content: [
+                'prev',
+                'title',
+                'close'
+            ]
+        }, {
+            position: 'bottom',
+            content: [
+                ''
+            ]
+        }]
+    });
+    $('.changemap').click(function() {
+        $('.address').fadeOut(500);
+    });
+    $('.changemap1').click(function() {
+        $('.address').fadeIn(500);
+    });
+    $(".che1").hide();
+    $(".playyoutube").click(function() {
+        $(".che1").fadeIn(500);
+        $(".lightboxright").html('<iframe width="100%" height="100%" src="https://www.youtube.com/embed/naI8SbkGrmw?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+        $(document).keyup(function(e) {
+            if (e.keyCode === 27) $('.hoancauclose').click(); // esc
+        });
+        return false;
+    });
+    $(".hoancauclose").click(function() {
+        $(".lightboxright").html('');
+        $(".che1").fadeOut(500);
+        return false;
+    });
+
+    function debounce(wait, bool, func) {
+        var timeout;
+        return function() {
+            var context = this,
+                args = arguments;
+            var executeFunction = function() {
+                func.apply(context, args);
+            };
+            if (bool == false) {
+                clearTimeout(timeout);
+                timeout = setTimeout(executeFunction, wait);
+            };
+            if (bool == true) {
+                func.apply(context, args);
+            };
+        }
+    };
+    var test = false;
+    $(window).scroll(debounce(1000, true, function() {
+        if (test == true) {
+            $("#menuNAV").removeClass("fix-scroll").hide().fadeOut(500);
+            $(".anhien").css("top", "0px");
+        }
+        console.log('dang lướt');
+    }));
+    $(window).scroll(debounce(1000, false, function() {
+        if ($(this).scrollTop() > 300) {
+            $("#menuNAV").hide().addClass("fix-scroll").fadeIn(500);
+            $(".anhien").css("top", "35px");
+            test = true;
+        } else {
+            $("#menuNAV").show().fadeIn(500);
+            $(".anhien").css("top", "0px");
+            test = true;
+        }
+        console.log('pause');
+    }));
+});
+</script>
+</body>
+</html>
