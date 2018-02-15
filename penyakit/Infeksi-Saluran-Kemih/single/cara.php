31ab2d94a35dfda61311e2c80404acc2e45389ad
agif <alpenizme@gmail.com>
2018-02-15 16:42:24 +0700

    New For Penyakit

---
+++ penyakit/Infeksi-Saluran-Kemih/single/cara.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Klinik Sentosa</title>
+    <meta name="language" content="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0,target-densitydpi=device-dpi, user-scalable=no">
+    <!-- <link href="http://dakhoahoancau.vn/img/favicon.png" rel="shortcut icon" type="image/x-icon"> -->
+    <meta name="geo.position" content="10.751806,106.684639">
+    <meta name="geo.placename" content="TPHCM">
+    <meta name="geo.region" content="VN-65">
+    <meta name="robots" content="noodp,index,follow">
+    <meta name="revisit-after" content="1 days">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta itemprop="name" content="">
+    <meta itemprop="description" content="">
+    <!-- Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="../../../css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/mdb.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/demo.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/jquery.mmenu.all.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/css.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/JS5.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/style.css">
+</head>
+<body>
+
+<?php include ('../../../layouts/header.php'); ?>
+        
+
+<div id="mm-0" class="mm-page mm-slideout">
+    <div class="w650">
+        <div class="banner">
+			<img class="img-fluid banner" src="../../../img/banner/doktor-1.jpg" alt="Klinik umum tim medis lengkap sempurna">
+		</div>
+        <section class="tranggiua m_content">
+            <div class="content_h">
+                <div class="m_motbenh">
+                    <div class="breadcrumb_main">
+                        <div class="breadcrumb">
+                            <a href="../../../" itemprop="url">
+                                <span itemprop="title" class="badge unique-color white-text z-depth-0">Home</span>
+                            </a>
+                            <span class="breadcrumb-span">
+                                <i class="fa fa-angle-right"></i>
+                            </span>
+                        </div>
+                        <!-- <div class="breadcrumb">
+                            <a href="../../" itemprop="url">
+                                <span itemprop="title" class="badge unique-color white-text z-depth-0">Penyakit</span>
+                            </a>
+                            <span class="breadcrumb-span">
+                                <i class="fa fa-angle-right"></i>
+                            </span>
+                        </div> -->
+                        <div class="breadcrumb">
+                            <a href="../" itemprop="url">
+                                <span itemprop="title" class="badge unique-color white-text z-depth-0">Infeksi Saluran Kemih</span>
+                            </a>
+                            <span class="breadcrumb-span">
+                                <i class="fa fa-angle-right"></i>
+                            </span>
+                        </div>
+                        <div class="breadcrumb">
+                            <a href="#in-here" itemprop="url">
+                                <span itemprop="title" class="badge unique-color white-text z-depth-0">Pengobatan</span>
+                            </a>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+                    <h4 class="h4-post h4-responsive"><i class="fa fa-th-large" aria-hidden="true"></i> Pengobatan Infeksi Saluran Kemih Pada Wanita</h4>
+                    <span class="post-time">
+                        Tanggal Diposting : 10-01-2018 - Tampilan : 667
+                    </span><br><br>
+                    <div class="content_chitiet">
+                        <p class="font-weight-bold">
+                            Bagaimana Cara Mengobati Penyakit Infeksi Saluran Kemih (ISK) Pada Wanita?
+                        </p>
+                        <p>
+                                Infeksi saluran kemih (ISK) yang terjadi pada wanita diobati secara efektif. Namun, seperti halnya pengobatan yang lainnya, sangat penting untuk menyelesaikan pengobatan sampai dinyatakan sembuh oleh dokter. Pengobatan untuk infeksi saluran kemih (ISK) pada wanita harus dirancang untuk setiap pasien secara individu dan biasanya didasarkan pada kondisi pasien, parasit yang dapat menyebabkan infeksi dan kerentanan parasit terhadap pengobatan.
+                        </p>
+                        <p>
+                                Mereka yang menderita infeksi saluran kemih (ISK) biasanya memiliki infeksi ginjal (pielonefritis) yang kemungkinan menyebar ke aliran darah. Orang yang kemungkinan memiliki infeksi yang lebih ringan (cystitis) dan bisa sembuh dengan cepat dengan pengobatan oral. Dan yang kemungkinan memiliki infeksi saluran kemih (ISK) yang disebabkan oleh parasit yang menyebabkan penyakit menular seksual (PMS) dan biasanya memerlukan lebih pengobatan oral.
+                        </p>
+                        <p>
+                                Pasien anak dan pasien hamil sebaiknya tidak menggunakan pengobatan oral tertentu yang biasa digunakan pada orang dewasa karena mempunyai efek samping. Pasien dengan infeksi saluran kemih (ISK) terkait STD atau seksual transmited disease (penyakit kelamin atau infeksi yang ditularkan melalui hubungan seksual, disebabkan oleh bakteri, virus dan parasit) biasanya memerlukan pengobatan untuk menghilangkan parasit STDatau seksual transmited disease (penyakit kelamin atau infeksi yang ditularkan melalui hubungan seksual, disebabkan oleh bakteri, virus dan parasit).
+                        </p>
+                        <p>
+                                Infeksi STD atau seksual transmited disease (penyakit kelamin atau infeksi yang ditularkan melalui hubungan seksual, disebabkan oleh bakteri, virus dan parasit)sering melibatkan lebih dari 1 organisme yang menginfeksi. Pasangan seks yang positif memiliki penyakit menular seksual (PMS) harus segera diobati meskipun tanda-tanda infeksi tidak ada. Parasit atau jamur yang kurang sering atau langka memerlukan pengobatan antijamur atau antiparasitik spesifik. Infeksi saluran kemih (ISK) yang lebih rumit ini seringkali harus ditangani dengan cara berkonsultasi dengan dokter spesialisnya.
+                        </p>
+                        <p class="special-type">
+                            <a href="#" style="text-align: center;"><span class="special-rgb"><strong><img src="../../../img/icon/special-icon.gif"></strong></span>
+                            </a><a href="#"><span style="color:#0000FF"><strong>>>> untuk lebih jelas?  </strong></span><span style="color:#FF0000"><strong> &gt;&gt;&gt; [klik disini untuk konsultasi] <<< </strong></span></a>
+                        </p>
+                        <p class="font-weight-bold">
+                            Pengobatan Yang Bertujuan Untuk Meredakan Gejala Infeksi Saluran Kemih
+                        </p>
+                        <p>
+                            Pada remaja yang sehat dan orang yang sudah dewasa dengan infeksi saluran kemih (ISK), maka pengobatan untuk infeksi saluran kemih (ISK) sangat diperlukan. Pada pria infeksi ini kemungkinan melibatkan kelenjar prostat dan pengobatan biasanya diberikan untuk durasi yang sedikit lebih lama. Tindak lanjut pengujian atau tes tidak diperlukan pada pria, pria yang  masih muda dan sehat atau wanita dengan infeksi kandung kemih (ISK) jika gejalanya sembuh.
+                        </p>
+                        <p>
+                            Serta pada wanita yang sedang hamil biasanya akan disarankan untuk memiliki kultur urine berulang sampai 1 atau 2 minggu setelah pengobatan berakhir untuk memastikan bakteri tidak lagi berada dalam air kencing. Pembakaran dengan buang air kecil (kencing) juga bisa terjadi pada wanita dengan infeksi pada vagina (seperti infeksi ragi) atau pada orang dengan urethritis (radang uretra atau saluran yang menghubungkan kantung kemih ke lingkungan luar tubuh). 
+                        </p>
+                    
+                        <p class="special-type">
+    						<a href="#" style="text-align: center;"><span class="special-rgb"><strong><img src="../../../img/icon/special-icon.gif"></strong></span>
+    						</a><a href="#"><span style="color:#0000FF"><strong>>>> butuh bantuan chat online </strong></span><span style="color:#FF0000"><strong> &gt;&gt;&gt; [klik disini untuk konsultasi] <<< </strong></span></a>
+                        </p>
+                        
+                        <p class="text-justify">
+                            <strong class="red-text"><b>Klinik Sentosa</b></strong> adalah salah satu klinik spesialis penyakit kulit dan kelamin yang berada di Jakarta, Indonesia. Menangani setiap permasalahan penyakit kulit dan kelamin seperti penyakit menular seksual (PMS), infeksi menular seksual (IMS), Andrologi dan Ginekologi. Didukung oleh dokter ahli dan peralatan medis yang modern guna membantu proses pengobatan  penyakit kelamin 
+                        </p>  
+                    </div>
+
+                    <blockquote class="blockquote bq-warning">
+                        <p class="text-muted">Disclaimer: Hasil dapat berbeda dari masing-masing individu.</p>
+                    </blockquote>
+
+                    <p class="jumbotron" style="color:red;">*Note: Efek pengobatan tergantung pada kondisi fisik masing-masing orang</p>
+                    <div class="clear20"></div>
+                    <ul class="tinlq list_doc">
+    					<div class="card card-header pinked-low mb-3">
+    						<h5 class="h5-responsive white-text">Posting Terkait</h5>
+    					</div>
+    					<li>
+    						<a href="gejala.php">
+    							<img class="img-thumbnail img-responsive img-fluid" src="../thumbnail/Gejala-Infeksi-Saluran-Kemih-Pada-Wanita.jpg"
+    							 align="left">
+    								Gejala Infeksi Saluran Kemih
+    						</a>
+    					</li>
+    					<hr>
+                        <li>
+    						<a href="klinik.php">
+    							<img class="img-thumbnail img-responsive img-fluid" src="../thumbnail/Klinik-Pengobatan-Infeksi-Saluran-Kemih-Pada-Wanita.jpg" align="left">
+    								Klinik Terbaik Yang Menangani Infeksi Saluran Kemih
+    						</a>
+    					</li>
+    					<hr>
+    					<li>
+    						<a href="biaya.php">
+    							<img class="img-thumbnail img-responsive img-fluid" src="../thumbnail/Biaya-Pengobatan-Infeksi-Saluran-Kemih-Pada-Wanita.jpg" align="left">
+    								Biaya Seputar Infeksi Saluran Kemih
+    						</a>
+    					</li>
+    					<hr>
+    					<li>
+    						<a href="penyebab.php">
+    							<img class="img-thumbnail img-responsive img-fluid" src="../thumbnail/Penyebab-Infeksi-Saluran-Kemih-Pada-Wanita.jpg"
+    							 align="left">
+    							 	Penyebab Infeksi Saluran Kemih
+    						</a>
+    					</li>
+                        <hr>
+                        <li>
+    						<a href="kencing-berbusa.php">
+    							<img class="img-thumbnail img-responsive img-fluid" src="../thumbnail/Kencing-Berbusa.jpg"
+    							 align="left">
+    							 	Kencing Berbusa
+    						</a>
+    					</li>
+    					<hr>
+    				</ul>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
+
+<?php include ('../../../layouts/footer.php'); ?>
+
+<script type="text/javascript" src="../../../js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="../../../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../../../js/popper.min.js"></script>
+<script type="text/javascript" src="../../../js/request.js"></script>
+<script type="text/javascript" src="../../../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../../../js/mdb.min.js"></script>
+<script type="text/javascript" src="../../../js/jquery.mmenu.min.all.js"></script>
+<script type="text/javascript" src="../../../js/style.js"></script>
+<script type="text/javascript">
+$(function() {
+    $('#menu').mmenu({
+        extensions: ['effect-slide-menu', 'penyakithadow'],
+        searchfield: false,
+        counters: true,
+        navbar: {
+            title: '<a href=".">Klinik Sentosa</a>'
+        },
+        navbars: [{
+            position: 'top',
+            content: [
+                'prev',
+                'title',
+                'close'
+            ]
+        }, {
+            position: 'bottom',
+            content: [
+                ''
+            ]
+        }]
+    });
+    $('.changemap').click(function() {
+        $('.address').fadeOut(500);
+    });
+    $('.changemap1').click(function() {
+        $('.address').fadeIn(500);
+    });
+    $(".che1").hide();
+    $(".playyoutube").click(function() {
+        $(".che1").fadeIn(500);
+        $(".lightboxright").html('<iframe width="100%" height="100%" src="https://www.youtube.com/embed/naI8SbkGrmw?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+        $(document).keyup(function(e) {
+            if (e.keyCode === 27) $('.hoancauclose').click(); // esc
+        });
+        return false;
+    });
+    $(".hoancauclose").click(function() {
+        $(".lightboxright").html('');
+        $(".che1").fadeOut(500);
+        return false;
+    });
+
+    function debounce(wait, bool, func) {
+        var timeout;
+        return function() {
+            var context = this,
+                args = arguments;
+            var executeFunction = function() {
+                func.apply(context, args);
+            };
+            if (bool == false) {
+                clearTimeout(timeout);
+                timeout = setTimeout(executeFunction, wait);
+            };
+            if (bool == true) {
+                func.apply(context, args);
+            };
+        }
+    };
+    var test = false;
+    $(window).scroll(debounce(1000, true, function() {
+        if (test == true) {
+            $("#menuNAV").removeClass("fix-scroll").hide().fadeOut(500);
+            $(".anhien").css("top", "0px");
+        }
+        console.log('dang lướt');
+    }));
+    $(window).scroll(debounce(1000, false, function() {
+        if ($(this).scrollTop() > 300) {
+            $("#menuNAV").hide().addClass("fix-scroll").fadeIn(500);
+            $(".anhien").css("top", "35px");
+            test = true;
+        } else {
+            $("#menuNAV").show().fadeIn(500);
+            $(".anhien").css("top", "0px");
+            test = true;
+        }
+        console.log('pause');
+    }));
+});
+</script>
+</body>
+</html>
