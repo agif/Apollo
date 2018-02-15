1b9d7d1e6034f0000926a3ae2dd939ef2bf820d9
agif <alpenizme@gmail.com>
2018-02-15 16:41:58 +0700

    New For Penyakit

---
+++ penyakit/Erosi-Serviks/single/penyebab.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Klinik Sentosa</title>
+    <meta name="language" content="en">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0,target-densitydpi=device-dpi, user-scalable=no">
+    <!-- <link href="http://dakhoahoancau.vn/img/favicon.png" rel="shortcut icon" type="image/x-icon"> -->
+    <meta name="geo.position" content="10.751806,106.684639">
+    <meta name="geo.placename" content="TPHCM">
+    <meta name="geo.region" content="VN-65">
+    <meta name="robots" content="noodp,index,follow">
+    <meta name="revisit-after" content="1 days">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    <meta itemprop="name" content="">
+    <meta itemprop="description" content="">
+    <!-- Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="../../../css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/mdb.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/demo.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/jquery.mmenu.all.min.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/css.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/JS5.css">
+    <link rel="stylesheet" type="text/css" href="../../../css/style.css">
+</head>
+<body>
+
+<?php include ('../../../layouts/header.php'); ?>
+        
+
+<div id="mm-0" class="mm-page mm-slideout">
+    <div class="w650">
+		<div class="banner">
+			<img class="img-fluid banner" src="../../../img/banner/doktor-1.jpg" alt="Klinik umum tim medis lengkap sempurna">
+		</div>
+        <section class="tranggiua m_content">
+        	<div class="content_h">
+				<div class="m_motbenh">
+					<div class="breadcrumb_main">
+						<div class="breadcrumb">
+							<a href="../../../" itemprop="url">
+								<span itemprop="title" class="badge unique-color white-text z-depth-0">Home</span>
+							</a>
+							<span class="breadcrumb-span">
+                                <i class="fa fa-angle-right"></i>
+                            </span>
+						</div>
+						<!-- <div class="breadcrumb">
+							<a href="../../" itemprop="url">
+								<span itemprop="title" class="badge unique-color white-text z-depth-0">Penyakit</span>
+							</a>
+							<span class="breadcrumb-span">
+                                <i class="fa fa-angle-right"></i>
+                            </span>
+						</div> -->
+						<div class="breadcrumb">
+							<a href="../" itemprop="url">
+								<span itemprop="title" class="badge unique-color white-text z-depth-0">Erosi-Serviks</span>
+							</a>
+							<span class="breadcrumb-span">
+                                <i class="fa fa-angle-right"></i>
+                            </span>
+						</div>
+						<div class="breadcrumb">
+							<a href="#in-here" itemprop="url">
+								<span itemprop="title" class="badge unique-color white-text z-depth-0">Penyebab</span>
+							</a>
+						</div>
+						<div class="clear"></div>
+					</div>
+					<h4 class="h4-post h4-responsive"><i class="fa fa-th-large" aria-hidden="true"></i> Penyebab Erosi Serviks</h4>
+					<span class="post-time">
+						Tanggal Diposting : 10-01-2018 - Tampilan : 667
+					</span><br><br>
+					<div class="content_chitiet">
+						<p class="font-weight-bold">
+							Apa Yang Menyebabkan Erosi Serviks?
+						</p>
+						<p>
+								Infeksi klamidia disebabkan oleh mikroorganisme parasit yang hidup. Seseorang biasanya tidak mengalami gejala apapun selama beberapa bulan ketika terjadi, gejalanya termasuk nyeri panggul (pelvic), demam, pendarahan, keputihan dan buang air kecil (kencing) yang tidak nyaman. Pada pria, infeksi biasanya ditandai dengan buang air kecil (kencing) yang menyakitkan yang  keluar dari penis.  
+						</p>
+						<p>
+								Klamidia perlu dilakukan pengobatan yang tepat sebelum menyebar ke tuba falopi atau rahim. Seorang wanita perlu menjalani pemeriksaan ginekologi secara teratur untuk mencegah penyakit lainnya yang kemungkinan akan muncul. Pemeriksaan tersebut meliputi pemeriksaan pap smear yang mendeteksi adanya sel abnormal pada permukaan serviks (leher rahim). 
+						</p>
+						<p>
+								Langkah kedua dari pemeriksaan ginekologi terdiri dari pemeriksaan tahunan organ reproduksi dan rektum (tempat penyimpanan sementara feses) untuk memeriksa adanya nyeri yang tertekan. Gonore biasanya tidak melibatkan gejala apapun sampai seseorang mengalami keputihan yang tidak biasa, nyeri pelvic (panggul), buang air kecil yang menyakitkan, demam dan pendarahan yang tidak biasa. 
+						</p>
+						<p>
+								Jika penyakitnya tidak diobati tepat waktu, gonore dapat menyebabkan masalah kesuburan, masalah kulit, keracunan darah, infeksi jantung, artritis ( radang sendi atau peradangan pada salah satu atau lebih , menyebabkan nyeri dan kekauan yang dapat memburuk seiring usia) dan infeksi otak. Reinfeksi terkadang bisa terjadi, jadi pasien harus sangat berhati-hati.
+						</p>
+						<p>
+								Trichomonas dipicu oleh organisme mikroskopik sederhana dan ditandai dengan buih berbusa dari vagina. Kadang-kadang juga termasuk kemerahan, nyeri, gatal dan bau yang tidak menyenangkan. Trichomonas jarang ada gejala pada pria namun dalam kasus yang jarang terjadi kemungkinan pasien merasa iritasi atau kemungkinan seperti sensasi terbakar saat buang air kecil (kencing).
+						</p>
+
+						<p class="special-type">
+							<a href="#" style="text-align: center;"><span class="special-rgb"><strong><img src="../../../img/icon/special-icon.gif"></strong></span>
+							</a><a href="#"><span style="color:#0000FF"><strong>>>> jika belum jelas mengenai penyebab erosi serviks </strong></span><span style="color:#FF0000"><strong> &gt;&gt;&gt; [klik disini] <<< </strong></span></a>
+						</p>
+						<p class="font-weight-bold">
+								Penyebab Umum Dari Erosi Serviks 
+						</p>
+
+						<p>
+								Penyebab umum dari erosi serviks adalah ketidakseimbangan hormon pada wanita. Hormon estrogen ini kebanyakan disalahkan untuk kondisi ini. Namun, akan ada yang namanya trauma, karena beberapa bahan kimia dan infeksi di kanal serviks sebagai penyebab erosi atau abrasi vagina. Ektopi serviks adalah umum selama pubertas, selama kehamilan dan pada wanita yang berada pada kontrol pil KB. Dimana hormon estrogen banyak terpengaruh dan berfluktuasi.
+						</p>
+						<p>
+								Penyebab masalah serviks (leher rahim) banyak dan bervariasi. Kemungkinan berkaitan dengan penyakit menular seksual (PMS) seperti gonore, sifilis atau klamidia. Dalam beberapa kasus persalinan yang sulit dapat menyebabkan infeksi. Yang menyebabkan erosi serviks tidak selalu jelas, gesekan hubungan intim nampaknya merupakan faktor sekaligus pil kontrasepsi.
+						</p>
+						<p>
+								Selain itu, diantara penyebab erosi berbagai infeksi seksual (khususnya mycoplasmosis, trikomoniasis atau klamidia), trauma mekanik pada selaput lendir serviks, serta gangguan hormonal pada tubuh wanita. Erosi serviks (leher rahim) sampai saat ini adalah penyakit paling umum pada bidang seksual wanita dan dapat menyebabkan berbagai komplikasi pada pengobatan dini (infeksi pada sistem urogenital, infertilitas atau kemandulan dan aborsi).
+						</p>
+						<p class="special-type">
+							<a href="#" style="text-align: center;"><span class="special-rgb"><strong><img src="../../../img/icon/special-icon.gif"></strong></span>
+							</a><a href="#"><span style="color:#0000FF"><strong>>>> tanya dokter untuk konsultasi  </strong></span><span style="color:#FF0000"><strong> &gt;&gt;&gt; [klik disini] <<< </strong></span></a>
+						</p>
+						<p class="font-weight-bold">
+								Kondisi Munculnya Penyebab Erosi Serviks
+						</p>
+						<p>
+								Penyebab utama erosi serviks adalah proses inflamasi asal menular. Epitel permukaan serviks (leher rahim) dapat menyebabkan mikroorganisme yang termasuk dalam infeksi urogenital, ureaplasma, mikoplasma, trikomoniasis, klamidia, virus herpes genital. Dalam beberapa kasus penyebab erosi serviks (leher rahim) kemungkinan merupakan pelanggaran terhadap sistem endoktrin, terutama fungsi kelenjar yang bertanggung jawab atas produksi hormon seks.
+						</p>
+						<p>
+								Kondisi yang diperlukan untuk munculnya erosi serviks (leher rahim)adalah penurunan lokal, dan dalam beberapa kasus dan kekebalan umum. Penurunan kekebalan yang ditandai saat wanita memiliki gaya hidup yang tidak sehat terutama pengaruh buruk adalah perubahan pasangan seksual yang sering dan sembarangan. Dengan semikian, cara hidup yang salah merupakan penyebab erosi serviks (leher rahim) yang sangat umum).
+						</p>
+
+						<p class="special-type">
+							<a href="#" style="text-align: center;"><span class="special-rgb"><strong><img src="../../../img/icon/special-icon.gif"></strong></span>
+							</a><a href="#"><span style="color:#0000FF"><strong>>>> butuh bantuan chat online  </strong></span><span style="color:#FF0000"><strong> &gt;&gt;&gt; [klik disini untuk konsultasi] <<< </strong></span></a>
+						</p>
+
+						<p class="text-justify">
+							<strong class="red-text"><b>Klinik Sentosa</b></strong> adalah salah satu klinik spesialis penyakit kulit dan kelamin yang berada di Jakarta, Indonesia. Menangani setiap permasalahan penyakit kulit dan kelamin seperti penyakit menular seksual (PMS), infeksi menular seksual (IMS), Andrologi dan Ginekologi. Didukung oleh dokter ahli dan peralatan medis yang modern guna membantu proses pengobatan  penyakit kelamin.
+						</p>	
+					</div>
+
+					<blockquote class="blockquote bq-warning">
+						<p class="text-muted">Disclaimer: Hasil dapat berbeda dari masing-masing individu.</p>
+					</blockquote>
+
+					<p class="jumbotron" style="color:red;">*Note: Efek pengobatan tergantung pada kondisi fisik masing-masing orang</p>
+					<div class="clear20"></div>
+					<ul class="tinlq list_doc">
+						<div class="card card-header pinked-low mb-3">
+							<h5 class="h5-responsive white-text">Posting Terkait</h5>
+						</div>
+						<li>
+							<a href="gejala.php">
+								<img class="img-thumbnail img-responsive img-fluid" src="../thumbnail/Gejala-Erosi-Serviks.jpg"
+								 align="left"> Gejala Erosi Serviks
+							</a>
+						</li>
+						<hr>
+						<li>
+							<a href="cara.php">
+								<img class="img-thumbnail img-responsive img-fluid" src="../thumbnail/Cara-Pengobatan-Erosi-Serviks.jpg" align="left">
+									Cara Mengangani Erosi Serviks
+							</a>
+						</li>
+						<hr>
+						<li>
+							<a href="klinik.php">
+								<img class="img-thumbnail img-responsive img-fluid" src="../thumbnail/Klinik-Pengobatan-Erosi-Serviks.jpg" align="left">
+									Klinik Terbaik Yang Menangani Erosi Serviks
+							</a>
+						</li>
+						<hr>
+						<li>
+							<a href="biaya.php">
+								<img class="img-thumbnail img-responsive img-fluid" src="../thumbnail/Biaya-Pengobatan-Erosi-Serviks.jpg" align="left">
+									Biaya Seputar Erosi Serviks
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</section>
+	</div>
+</div>
+
+<?php include ('../../../layouts/footer.php'); ?>
+
+<script type="text/javascript" src="../../../js/jquery-3.2.1.min.js"></script>
+<script type="text/javascript" src="../../../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../../../js/popper.min.js"></script>
+<script type="text/javascript" src="../../../js/request.js"></script>
+<script type="text/javascript" src="../../../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../../../js/mdb.min.js"></script>
+<script type="text/javascript" src="../../../js/jquery.mmenu.min.all.js"></script>
+<script type="text/javascript" src="../../../js/style.js"></script>
+<script type="text/javascript">
+$(function() {
+    $('#menu').mmenu({
+        extensions: ['effect-slide-menu', 'penyakithadow'],
+        searchfield: false,
+        counters: true,
+        navbar: {
+            title: '<a href=".">Klinik Sentosa</a>'
+        },
+        navbars: [{
+            position: 'top',
+            content: [
+                'prev',
+                'title',
+                'close'
+            ]
+        }, {
+            position: 'bottom',
+            content: [
+                ''
+            ]
+        }]
+    });
+    $('.changemap').click(function() {
+        $('.address').fadeOut(500);
+    });
+    $('.changemap1').click(function() {
+        $('.address').fadeIn(500);
+    });
+    $(".che1").hide();
+    $(".playyoutube").click(function() {
+        $(".che1").fadeIn(500);
+        $(".lightboxright").html('<iframe width="100%" height="100%" src="https://www.youtube.com/embed/naI8SbkGrmw?autoplay=1" frameborder="0" allowfullscreen></iframe>');
+        $(document).keyup(function(e) {
+            if (e.keyCode === 27) $('.hoancauclose').click(); // esc
+        });
+        return false;
+    });
+    $(".hoancauclose").click(function() {
+        $(".lightboxright").html('');
+        $(".che1").fadeOut(500);
+        return false;
+    });
+
+    function debounce(wait, bool, func) {
+        var timeout;
+        return function() {
+            var context = this,
+                args = arguments;
+            var executeFunction = function() {
+                func.apply(context, args);
+            };
+            if (bool == false) {
+                clearTimeout(timeout);
+                timeout = setTimeout(executeFunction, wait);
+            };
+            if (bool == true) {
+                func.apply(context, args);
+            };
+        }
+    };
+    var test = false;
+    $(window).scroll(debounce(1000, true, function() {
+        if (test == true) {
+            $("#menuNAV").removeClass("fix-scroll").hide().fadeOut(500);
+            $(".anhien").css("top", "0px");
+        }
+        console.log('dang lướt');
+    }));
+    $(window).scroll(debounce(1000, false, function() {
+        if ($(this).scrollTop() > 300) {
+            $("#menuNAV").hide().addClass("fix-scroll").fadeIn(500);
+            $(".anhien").css("top", "35px");
+            test = true;
+        } else {
+            $("#menuNAV").show().fadeIn(500);
+            $(".anhien").css("top", "0px");
+            test = true;
+        }
+        console.log('pause');
+    }));
+});
+</script>
+</body>
+</html>
