21033c62c7b49213a284d8db84471d16b55666c8
agif <alpenizme@gmail.com>
2018-02-26 15:44:55 +0700

    commit

--- layouts/banner.php
+++
@@ -1,7 +0,0 @@
-<div id="mm-0" class="mm-page mm-slideout">
-	<div class="w650">
-		<div class="banner">
-		    <img class="img-fluid banner" src="img/banner/doktor-1.jpg" alt="Klinik umum tim medis lengkap sempurna">
-		</div>
-	</div>
-</div>
